1f30adade8ae4dd2acd2285614cf2834eec68070
Xeruvimov <xeruvimov@gmail.com>
2019-05-03 13:12:30 +0400

    LabFive is done/n*Need repair work with memory

---
+++ lists/LoopLinkedList.cpp
@@ -0,0 +1,92 @@
+#include <stdexcept>
+#include "../LabTwo.cpp"
+#include "nodes.h"
+
+using namespace std;
+
+//todo А ПОЧЕМУ РОТ В ПАМЯТИ? Нужны деструкторы
+struct LoopLinkedList {
+public:
+    void push(SomeStruct c) {
+        DoubleLinkNode *newNode = new DoubleLinkNode;
+        newNode->data = c;
+
+        if (this->size == 0) {
+            this->head = newNode;
+            this->tail = this->head;
+        } else {
+            this->tail->next = newNode;
+            newNode->prev = this->tail;
+            this->tail = newNode;
+        }
+
+        this->size++;
+    }
+
+    void deleteLast() {
+        if (this->size == 0) {
+            throw runtime_error("Delete from empty LinkedList");
+        }
+
+        if (this->size == 1) {
+            delete head;
+            this->head = nullptr;
+            this->tail = this->head;
+        } else {
+            this->tail = this->tail->prev;
+            delete this->tail->next;
+        }
+        this->size--;
+    }
+
+    struct Iterator {
+        DoubleLinkNode *nextNode;
+        DoubleLinkNode *prevNode;
+
+        explicit Iterator(LoopLinkedList &loopLinkedList) {
+            this->nextNode = loopLinkedList.getHead();
+            this->prevNode = NULL;
+        }
+
+        bool hasNext() {
+            return this->nextNode != NULL;
+        }
+
+        bool hasPrevious() {
+            return this->prevNode != NULL;
+        }
+
+        SomeStruct next() {
+            if (this->nextNode == NULL) {
+                throw runtime_error("No such element exception");
+            }
+            SomeStruct result = this->nextNode->data;
+            this->prevNode = this->nextNode;
+            this->nextNode = this->nextNode->next;
+            return result;
+        }
+
+        SomeStruct previous() {
+            if (this->prevNode == NULL) {
+                throw runtime_error("No such element exception");
+            }
+            SomeStruct result = this->prevNode->data;
+            this->nextNode = this->prevNode;
+            this->prevNode = this->prevNode->prev;
+            return result;
+        }
+    };
+
+    Iterator *getIterator() {
+        return new Iterator(*this);
+    }
+
+private:
+    size_t size = 0;
+    DoubleLinkNode *head = nullptr;
+    DoubleLinkNode *tail;
+
+    DoubleLinkNode *getHead() {
+        return this->head;
+    }
+};
---
+++ lists/SingleLinkedList.cpp
@@ -0,0 +1,71 @@
+#include <stdexcept>
+
+using namespace std;
+
+//todo ОПЯТЬ ВЕСЬ РОТ В ПАМЯТИ
+struct SinglLinkedList {
+public:
+    void push(SomeStruct c) {
+        auto *newNode = new LinkNode;
+        newNode->data = c;
+
+        if (this->size == 0) {
+            this->head = newNode;
+            this->tail = this->head;
+        } else {
+            this->tail->next = newNode;
+            this->tail = newNode;
+        }
+
+        this->size++;
+    }
+
+    void deleteLast() {
+        if (this->size == 0) {
+            throw runtime_error("Delete from empty LinkedList");
+        }
+
+        if (this->size == 1) {
+            delete head;
+            this->head = nullptr;
+            this->tail = this->head;
+        } else {
+            delete this->tail->next;
+        }
+        this->size--;
+    }
+
+    struct Iterator {
+        LinkNode *nextNode;
+
+        explicit Iterator(SinglLinkedList &singlLinkedList) {
+            this->nextNode = singlLinkedList.getHead();
+        }
+
+        bool hasNext() {
+            return this->nextNode != NULL;
+        }
+
+        SomeStruct next() {
+            if (this->nextNode == NULL) {
+                throw runtime_error("No such element exception");
+            }
+            SomeStruct result = this->nextNode->data;
+            this->nextNode = this->nextNode->next;
+            return result;
+        }
+    };
+
+    Iterator *getIterator() {
+        return new Iterator(*this);
+    }
+
+private:
+    size_t size = 0;
+    LinkNode *head = nullptr;
+    LinkNode *tail;
+
+    LinkNode *getHead() {
+        return this->head;
+    }
+};
